659688bfb5beb8f01bfc5690449ed7e65fbd6d64
Echo0701 <133868458+Echo0701@users.noreply.github.com>
2024-01-06 18:53:38 +0800

    文章详情代码实现

--- blog-front/src/main/java/com/myblog/controller/ArticleController.java
+++ blog-front/src/main/java/com/myblog/controller/ArticleController.java
@@ -5,6 +5,7 @@ import com.myblog.domain.ResponseResult;
 import com.myblog.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -54,5 +55,18 @@ public class ArticleController {
     public ResponseResult articleList(Integer pageNum, Integer pageSize, Long categoryId) {
         return articleService.articleList(pageNum, pageSize, categoryId);
     }
+
+    // --------------------- 查询文章详情 -----------------------------------------
+
+    /**
+     * 根据 id 查询文章详情
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public ResponseResult getArticleDetail(@PathVariable("id") Long id) {
+        return articleService.getArticleDetail(id);
+    }
+
 
 }
